16100e21e10c85127f526591d35cfd92d1b12ce1
mrsbolinhu <ilanavirginiab@gmail.com>
2024-03-19 19:58:03 -0300

    fix(workflows): adjust paths

--- src/components/galleryCard.svelte
+++ src/components/galleryCard.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { base } from "$app/paths";
 	import Modal from './modal.svelte';
 
 	export let picture;
@@ -7,7 +8,7 @@
 </script>
 
 <button on:click={handlePictureClick} class="w-full h-full">
-	<img class="w-full h-full rounded-lg object-cover" src={picture.path} alt={picture.description} />
+	<img class="w-full h-full rounded-lg object-cover" src={base + picture.path} alt={picture.description} />
 </button>
 
 <Modal {showModal} {picture} {handlePictureClick} />
